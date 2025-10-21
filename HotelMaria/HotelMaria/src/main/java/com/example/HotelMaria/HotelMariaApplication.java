5ed11815be0a0199cb8da7c81d56d0520cf64c69
MAYARA LUPETI TURBIANI <mayara.lturbiani@senacsp.edu.br>
2025-10-20 21:13:18 -0300

    estrutura inicial do projeto

---
+++ HotelMaria/HotelMaria/src/main/java/com/example/HotelMaria/HotelMariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelMaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMariaApplication.class, args);
+	}
+
+}
---
+++ HotelMaria/HotelMaria/src/test/java/com/example/HotelMaria/HotelMariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelMaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
